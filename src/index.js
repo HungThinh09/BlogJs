289cd8b853f9d04bd80541c3753a5c8ec756d506
Hung <hungtph03027@gmail.com>
2022-07-17 14:53:32 +0700

    Upload Blogjs

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const path = require('path')
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'))
+//template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views',path.join(__dirname,'resources/views'))
+
+app.get('/', (req, res) => {
+   res.render('home')
+})
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
